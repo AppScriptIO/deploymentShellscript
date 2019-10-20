9647a6d852c6758f1c09d54cf92553fba92ccaf3
Safi <nassarsafi@gmail.com>
2019-10-20 17:10:49 +0300

    Minor script change.

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ git stash save -- "checkout stash before release."
 git branch distribution
 git checkout distribution && git rebase --onto master distribution
 # find . -name distribution -prune -o -print0 | xargs -0 rm -r
-rm ./.git/info/exclude && mv ./.gitignore ./.git/info/exclude # deleting .gitignore will make it faster, by preventing node_modules from being processed by tools while deleting files.
+rm ./.git/info/exclude && mv ./.gitignore ./.git/info/exclude || mv ./configuration/.gitignore ./.git/info/exclude # deleting .gitignore will make it faster, by preventing node_modules from being processed by tools while deleting files.
 find . -path ./distribution -prune -o -path ./.git -prune -o -path ./node_modules -prune -o -exec rm -rf {} \; 2> /dev/null
 cp -r ./distribution/* .
 git add -A && git commit -a -m '🏗 Release.' && git tag $version; 
