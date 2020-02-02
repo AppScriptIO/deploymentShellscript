79554dd55a0070f0e300236d9fdd804299aab06f
Safi <nassarsafi@gmail.com>
2020-02-02 15:31:44 +0200

    ➕ Migrate deprecated scripts from portainer container scripts.

---
+++ entrypointScripts and other/portainerContainerManagementUI.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Portainer is a docker management ui.
+docker-compose \
+        -f ./portainerContainerGUI.dockerCompose.yml \
+        --project-name containerGUI \
+        up -d --force-recreate
+
+docker ps
+
+# open browser
+rundll32 url.dll,FileProtocolHandler http://localhost:9000
